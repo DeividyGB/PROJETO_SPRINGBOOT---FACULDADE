1be9650a6e9e6156c3dc9215acac7dcadd7108d7
Deividy <deividygustavo2016@gmail.com>
2025-02-20 22:35:28 -0300

    Enviando o parametro

--- src/main/java/com/programacao/web/fatec/api_fatec/controllers/controller.java
+++ src/main/java/com/programacao/web/fatec/api_fatec/controllers/controller.java
@@ -2,12 +2,13 @@ package com.programacao.web.fatec.api_fatec.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class controller {
 
-    @GetMapping
-    public String HelloWorld(){
-        return "Hello";
+    @GetMapping("{name}")
+    public String HelloWorld(@PathVariable String name){
+        return name;
     }
 }
